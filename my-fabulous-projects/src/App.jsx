f4d1a04c4cf065065d4bc88aee15440b9195c859
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-07-15 12:12:10 +0200

    refactor: remove Events page and update CSS variables for improved styling

--- my-fabulous-projects/src/App.jsx
+++ my-fabulous-projects/src/App.jsx
@@ -3,7 +3,6 @@ import Nav from './components/Nav'
 import Home from './pages/Home'
 import About from './pages/About'
 import Portfolio from './pages/Portfolio'
-import Events from './pages/Events'
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/events" element={<Events />} />
       </Routes>
     </HashRouter>
   )
